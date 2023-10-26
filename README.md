f4be4b8774647fed901c6f384e1232c9bfd58a80
James Colvin <jamesc127@gmail.com>
2023-10-26 15:19:47 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # AI-Choose-Your-Own-Adventure-Game
+## Setup
+1. Copy your Astra DB auth JSON file into this directory and rename `token.json`.
+2. Copy your Astra DB secure connect bundle into this directory and rename `scb.zip`.
+3. Within your Python env of choice, run
+    ```Python
+    pip install chainlit cassio cassandra-driver langchain  openai
+    ```
+4. To load the UI, run
+    ```Python
+    chainlit run tutorial.py
+    ```
--- tutorial.py
+++ tutorial.py
@@ -3,27 +3,28 @@ from cassandra.auth import PlainTextAuthProvider
 from langchain.memory import CassandraChatMessageHistory, ConversationBufferMemory
 from langchain.llms import OpenAI
 from langchain import LLMChain, PromptTemplate
+import chainlit as cl
 import json
 
 cloud_config= {
-  'secure_connect_bundle': 'secure-connect-choose-your-own-adventure.zip'
+  'secure_connect_bundle': 'scb.zip'
 }
 
-with open("choose_your_own_adventure-token.json") as f:
+with open("token.json") as f:
     secrets = json.load(f)
 
 CLIENT_ID = secrets["clientId"]
 CLIENT_SECRET = secrets["secret"]
-ASTRA_DB_KEYSPACE = ""
-OPENAI_API_KEY = ""
+ASTRA_DB_KEYSPACE = "live"
+OPENAI_API_KEY = secrets["openai"]
 
 auth_provider = PlainTextAuthProvider(CLIENT_ID, CLIENT_SECRET)
 cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
-session = cluster.connect()
+cassSession = cluster.connect()
 
 message_history = CassandraChatMessageHistory(
     session_id="anything",
-    session=session,
+    session=cassSession,
     keyspace=ASTRA_DB_KEYSPACE,
     ttl_seconds=3600
 )
@@ -44,33 +45,32 @@ Your goal is to create a branching narrative experience where each choice
 leads to a new path, ultimately determining Elara's fate. 
 
 Here are some rules to follow:
-1. Start by asking the player to choose some kind of weapons that will be used later in the game
+1. Start by asking the player to choose some kind of weapon that will be used later in the game
 2. Have a few paths that lead to success
 3. Have some paths that lead to death. If the user dies generate a response that explains the death and ends in the text: "The End.", I will search for this text to end the game
 
 Here is the chat history, use this to understand what to say next: {chat_history}
 Human: {human_input}
 AI:"""
+@cl.on_chat_start
+async def main():
+    prompt = PromptTemplate(
+        input_variables=["chat_history", "human_input"],
+        template=template
+    )
+    llm = OpenAI(openai_api_key=OPENAI_API_KEY)
+    llm_chain = LLMChain(
+        llm=llm,
+        prompt=prompt,
+        memory=cass_buff_memory,
+        verbose=True
+    )
+    cl.user_session.set("llm_chain", llm_chain)
+    await cl.Message("Welcome to Astra DB Choose Your Own Adventure. Type 'start' to begin.").send()
 
-prompt = PromptTemplate(
-    input_variables=["chat_history", "human_input"],
-    template=template
-)
-
-llm = OpenAI(openai_api_key=OPENAI_API_KEY)
-llm_chain = LLMChain(
-    llm=llm,
-    prompt=prompt,
-    memory=cass_buff_memory
-)
-
-choice = "start"
-
-while True:
-    response = llm_chain.predict(human_input=choice)
-    print(response.strip())
-
-    if "The End." in response:
-        break
-
-    choice = input("Your reply: ")
+    
+@cl.on_message
+async def main(choice: cl.Message):
+    llm_chain = cl.user_session.get("llm_chain")
+    response = await llm_chain.acall(choice.content, callbacks=[cl.AsyncLangchainCallbackHandler()])
+    await cl.Message(content=response["text"]).send()
